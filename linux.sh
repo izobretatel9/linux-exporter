f58bc5b5137fdc340c3a908c8ea1b0d67b4155d4
Vadim Maltsev <fedos550@mail.ru>
2021-10-16 13:19:50 +0300

    Rename

--- linux.sh
+++ linux.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz # Заменить на актуальную версию
+#Downloading
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz # Replace on new verison
 tar -xvzf node_exporter-1.1.2.linux-amd64.tar.gz
 cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin/
 /usr/sbin/useradd --no-create-home --shell /bin/false node_exporter
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
-#Добавляем
+#Add
 tee /etc/systemd/system/node_exporter.service <<"EOF"
 [Unit]
 Description=Node Exporter
@@ -18,16 +19,16 @@ ExecStart=/usr/local/bin/node_exporter $OPTIONS
 [Install]
 WantedBy=multi-user.target
 EOF
-#Создаем    
+#Create    
 mkdir /etc/sysconfig
 tee /etc/sysconfig/node_exporter <<"EOF"
 OPTIONS="--collector.disable-defaults --collector.cpu --collector.cpufreq --collector.diskstats --collector.meminfo --collector.filesystem --collector.netdev"
 EOF
 chown node_exporter:node_exporter /etc/sysconfig/node_exporter
-#Удаляем
+#Delete
 rm node_exporter-1.1.2.linux-amd64.tar.gz
 rm -rf node_exporter-1.1.2.linux-amd64
-#Запускаем
+#Run
 systemctl daemon-reload && \
 systemctl enable node_exporter && \
 systemctl start node_exporter && \
